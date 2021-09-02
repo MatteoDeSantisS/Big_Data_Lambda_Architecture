6aa92f4b6f68ca64561675312b203ba9cb369de7
kai0cesar <kaiocesar.alvesreis@gmail.com>
2021-09-02 16:21:07 +0200

    Modifiche

--- kafka/kafka-producer.py
+++ kafka/kafka-producer.py
@@ -10,12 +10,12 @@ pollution = csv.values.tolist()
 producer = KafkaProducer(bootstrap_servers='localhost:9092', value_serializer = lambda x: json.dumps(x).encode('utf-8'))
 
 while True:
-    current_date = datetime.strptime(pollution[0][4], "%Y-%m-%d")
+    current_date = datetime.strptime(pollution[0][5], "%Y-%m-%d")
 
     for record in pollution:
-        if(current_date == datetime.strptime(record[4], "%Y-%m-%d")):
+        if(current_date == datetime.strptime(record[5], "%Y-%m-%d")):
             record = tuple(record)
-            to_send = {
+            data_to_send = {
                 'State Code': record[0],
                 'Address': record[1],
                 'State': record[2],
@@ -30,7 +30,7 @@ while True:
             producer.send('sensors-data', value = data_to_send)
         else:
             time.sleep(10)
-            current_date = datetime.strptime(record[4], "%Y-%m-%d")
+            current_date = datetime.strptime(record[5], "%Y-%m-%d")
             record = tuple(record)
             data_to_send = {
                 'State Code': record[0],
